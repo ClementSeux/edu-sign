ac2c7e820b2ee3bb0ddd507bddfc9f0eaf74a8d9
anthony-mini <anthony-mini@outlook.fr>
2023-09-12 14:59:28 +0200

    Add DropDown choice

--- front/src/components/Login/index.jsx
+++ front/src/components/Login/index.jsx
@@ -1,13 +1,34 @@
 import axios from 'axios';
+import { useState } from 'react';
+
+function ProfilDropDown() {
+  const [choix, setChoix] = useState('choix1'); // Par défaut, vous pouvez sélectionner une option
+
+  const handleChoixChange = (e) => {
+    setChoix(e.target.value);
+  };
+  return (
+    <div>
+      <label htmlFor="choix">Connectez vous à votre profil enseignant :</label>
+      <select id="choix" value={choix} onChange={handleChoixChange}>
+        <option value="choix1">Enseignants</option>
+        <option value="choix2">Albus Dumbledore</option>
+        <option value="choix3">Minerva McGonagall</option>
+        <option value="choix4">Severus Rogue</option>
+      </select>
+    </div>
+  );
+}
 
 export default function Login() {
   axios
-    .get('http://localhost:8000/exemple')
+    .get('http://localhost:8000/example')
     .then((response) => console.log('data', response));
 
   return (
     <>
       <h2>Login</h2>
+      <ProfilDropDown />
     </>
   );
 }
