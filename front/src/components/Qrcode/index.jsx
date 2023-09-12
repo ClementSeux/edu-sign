07fe4e10b7e14d6cb0aceb541be5c5ceb26f0231
anthony-mini <anthony-mini@outlook.fr>
2023-09-12 21:45:52 +0200

    Modif structure

--- front/src/components/Qrcode/index.jsx
+++ front/src/components/Qrcode/index.jsx
@@ -1,34 +1,34 @@
-import React, { useState, useEffect }  from "react";
-import axios from "axios";
-import QRCode from "react-qr-code";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import QRCode from 'react-qr-code';
 
 export default function Qrcode() {
-
-  const [hash, setHash] = useState("hey");
+  const [hash, setHash] = useState('hey');
 
   //const getHash = async () => {
-    // await axios
-    // .get('http://localhost:8000/qrcode')
-    // .then((response) => {
-    //   console.log(response.data);
-    //   setHash(response.data.hash);
-    // });
+  // await axios
+  // .get('http://localhost:8000/qrcode')
+  // .then((response) => {
+  //   console.log(response.data);
+  //   setHash(response.data.hash);
+  // });
   //}
 
   const getQrCode = () => {
-
     // getHash();
-    
 
-    return(
-      hash && <QRCode value={hash} />
-    ) 
-  }
+    return hash && <QRCode value={hash} />;
+  };
 
   return (
-    <>
-      <h2>Scannez ce code</h2>
-      <div id="qrcode"> {getQrCode()}</div>
-    </>
+    <section className="bloc-card">
+      <div className="container-card">
+        <div id="qrcode" className="card-qr">
+          {' '}
+          {getQrCode()}
+        </div>
+        <h2>Scannez ce code</h2>
+      </div>
+    </section>
   );
 }
