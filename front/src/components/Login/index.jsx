8db38e03218024deb73c336251c0062776eeb8b9
anthony-mini <anthony-mini@outlook.fr>
2023-09-13 10:04:17 +0200

    Call des données utilisateurs présents sur l'API

--- front/src/components/Login/index.jsx
+++ front/src/components/Login/index.jsx
@@ -1,47 +1,65 @@
-import axios from 'axios';
-import { useState } from 'react';
+// import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 /****** Retrieving information via API ******/
 
 // axios
 //   .get('http://localhost:8000/example')
-  //.then((response) => console.log('data', response));
+//.then((response) => console.log('data', response));
 
-/******* DROPDOWN  *******/
+export default function Login() {
+  const [APIState, setAPIState] = useState({
+    loading: false,
+    error: false,
+    data: undefined,
+  });
 
-function ProfilDropDown() {
+  useEffect(() => {
+    // Utilisez une fonction séparée pour effectuer l'appel API dans useEffect
+    const fetchData = async () => {
+      setAPIState({ ...APIState, loading: true });
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users',
+        );
+        console.log(response);
+        if (!response.ok) {
+          console.log(`500`);
+          throw new Error('Server Error');
+        }
+        const data = await response.json();
+        console.log(data);
+        setAPIState({ loading: false, error: false, data });
+      } catch (error) {
+        setAPIState({ loading: false, error: true, data: undefined });
+        console.log(`404`);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const [choix, setChoix] = useState('choix1'); // Par défaut, vous pouvez sélectionner une option
 
   const handleChoixChange = (e) => {
     setChoix(e.target.value);
   };
-  return (
-    <div>
-      <label htmlFor="choix">Connectez vous à votre profil enseignant :</label>
-      <select id="choix" value={choix} onChange={handleChoixChange}>
-        <option value="choix1">Enseignants</option>
-        <option value="choix2">Albus Dumbledore</option>
-        <option value="choix3">Minerva McGonagall</option>
-        <option value="choix4">Severus Rogue</option>
-      </select>
-    </div>
-  );
-}
 
-/***** AUTHENTIFACTION BUTTON ******/
-
-function AuthButton() {
-    return(
-        <button>Authentification</button>
-    )
-}
-
-export default function Login() {
   return (
     <>
       <h2>Login</h2>
-      <ProfilDropDown />
-      <AuthButton />
+      <div>
+        <label htmlFor="choix">
+          Connectez vous à votre profil enseignant :
+        </label>
+        <select id="choix" value={choix} onChange={handleChoixChange}>
+          <option value="choix1">Enseignants</option>
+          <option value="choix2">Albus Dumbledore</option>
+          <option value="choix3">Minerva McGonagall</option>
+          <option value="choix4">Severus Rogue</option>
+        </select>
+      </div>
+      <button>Authentification</button>
     </>
   );
 }
