9e53687534d884000e309ebb80b94ffffaeb36f7
anthony-mini <anthony-mini@outlook.fr>
2023-09-13 21:29:34 +0200

    Fix @fonawesome build error

--- front/src/components/Header/index.jsx
+++ front/src/components/Header/index.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faBookOpenReader, faCircleCheck, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
-import { useLocalStorage } from "@uidotdev/usehooks";
-import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBookOpenReader,
+  faCircleCheck,
+  faCircleXmark,
+} from '@fortawesome/free-solid-svg-icons';
+import { useLocalStorage } from '@uidotdev/usehooks';
 
 export default function Header() {
   const [token, setToken] = useLocalStorage('token', null);
   return (
     <header>
-        {/* <FontAwesomeIcon className="icon-book" icon={faBookOpenReader} />
-        <FontAwesomeIcon className='icon-check__true' icon={faCircleCheck} />
-        <FontAwesomeIcon className='icon-check__false' icon={faCircleXmark} /> */}
-        <h1>Edu-sign</h1>
-        {token && <p>{token}</p>}
+      {token ? (
+        <>
+          <FontAwesomeIcon className="icon-check__true" icon={faCircleCheck} />
+          <p className="icon-book__alertTrue">Déconnexion</p>
+        </>
+      ) : (
+        <>
+          <FontAwesomeIcon className="icon-check__false" icon={faCircleXmark} />
+          <p className="icon-book__alertFalse">Connexion</p>
+        </>
+      )}
+      <button
+        onClick={() => {
+          window.localStorage.clear();
+          window.location.reload();
+          window.alert('Utilisateur déconnecté');
+        }}
+      >
+        <FontAwesomeIcon className="icon-book" icon={faBookOpenReader} />
+      </button>
+      <h1>Edu-sign</h1>
     </header>
   );
 }
