f962e1f8214b450c20fcdf720346dbfac23f9e74
anthony-mini <anthony-mini@outlook.fr>
2023-09-13 15:53:23 +0200

    update loginStatus

--- front/src/components/Header/index.jsx
+++ front/src/components/Header/index.jsx
@@ -10,14 +10,19 @@ export default function Header() {
   const [token, setToken] = useLocalStorage('token', null);
   return (
     <header>
-      <>
-        {token ? (
-          <FontAwesomeIcon className="icon-check__true" icon={faCircleCheck} />
-        ) : (
-          <FontAwesomeIcon className="icon-check__false" icon={faCircleXmark} />
-        )}
-      </>
-      <FontAwesomeIcon className="icon-book" icon={faBookOpenReader} />
+      {token ? (
+        <FontAwesomeIcon className="icon-check__true" icon={faCircleCheck} />
+      ) : (
+        <FontAwesomeIcon className="icon-check__false" icon={faCircleXmark} />
+      )}
+      <button
+        onClick={() => {
+          window.localStorage.clear();
+          window.location.reload();
+        }}
+      >
+        <FontAwesomeIcon className="icon-book" icon={faBookOpenReader} />
+      </button>
       <h1>Edu-sign</h1>
     </header>
   );
