d89d30f9f8f08959eb19a20b6846ff2a94d139fc
anthony-mini <anthony-mini@outlook.fr>
2023-09-13 11:56:32 +0200

    push

--- front/src/components/Header/index.jsx
+++ front/src/components/Header/index.jsx
@@ -1,3 +1,16 @@
+import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookOpenReader } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
 export default function Header() {
-  return <h1>Edu-sign</h1>;
+  return (
+    <header>
+      <Link to="/">
+        <FontAwesomeIcon className="icon" icon={faBookOpenReader} />
+      </Link>
+      <h1>Edu-sign</h1>
+    </header>
+  );
 }
