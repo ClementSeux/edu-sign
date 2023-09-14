78698fe5e8d016957e302ed9011db6dc8a63e63a
anthony-mini <anthony-mini@outlook.fr>
2023-09-14 16:55:02 +0200

    Update Dashboard Page

--- front/src/pages/Dashboard/index.jsx
+++ front/src/pages/Dashboard/index.jsx
@@ -1,15 +1,36 @@
-import { Header, Login } from '../../components/index';
+import { Header } from '../../components/index';
+import { BannerTeach, BannerStudents } from '../../assets';
 
-export default function Dashboard({handleTokenReception}) {
+export default function Dashboard() {
   const handlerTokenReception = (token) => {
-    handleTokenReception(token)
-    console.log('page send token :', token)
-  }
+    handleTokenReception(token);
+    console.log('page send token :', token);
+  };
 
   return (
     <>
       <Header />
-      <Login handleTokenReception={handlerTokenReception}/>{' '}
+      <h2 className='dashboard-title'>
+        5x plus rapide ! 
+        <ul>
+          <li>Assiduité</li>
+          <li>Automatisater vos documents</li>
+          <li>Analyse de l'apprentissage</li>
+          <li>Alerte & gestion des cas</li>
+        </ul>
+      </h2>
+      <a href="/login" className="banner-teachers">
+        <h3 className="banner-teachers__title">Connexion intervenant</h3>
+        <picture className="banner-teachers__img">
+          <img src={BannerTeach} alt="Teacher classroom" />
+        </picture>
+      </a>
+      <a href="/login" className="banner-students">
+        <h3 className="banner-students__title">Connexion apprenant</h3>
+        <picture className="banner-students__img">
+          <img src={BannerStudents} alt="students classroom" />
+        </picture>
+      </a>
     </>
   );
 }
