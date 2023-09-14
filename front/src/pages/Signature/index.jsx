f2d2679ebaaa13830a06da36fcef28a631b7ba34
FerretDad <clement.seux@gmail.com>
2023-09-14 10:11:11 +0200

    personnalised token query

--- front/src/pages/Signature/index.jsx
+++ front/src/pages/Signature/index.jsx
@@ -5,44 +5,7 @@ import axios from 'axios';
 
 export default function Signature() {
 
-  const getHash = () => {
-    const queryParameters = new URLSearchParams(window.location.search)
-    const hash = queryParameters.get("hash")
-    console.log(hash)
-    return hash
-  }
   
-  const postRequest = async (hash) => {
-    let data = {
-      'hash': hash
-    };
-    let result = [];
-
-    let config = {
-      method: "post",
-      maxBodyLength: Infinity,
-      url: BACK_HOST + "/verify",
-      headers: {},
-      data: data,
-    };
-
-    await axios
-      .request(config)
-      .then((response) => {
-        result = response.data.response;
-        console.log(response.data)
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    return result;
-  };
-  
-  useEffect( () => {
-    const hash = getHash()
-    postRequest(hash).then((response)=>console.log('response de l api verify', response))
-     
-      },[] );
 
   return (
     <>
