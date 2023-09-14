f799ce2e3c9ef9b0193a6dde47db9810265f1aff
anthony-mini <anthony-mini@outlook.fr>
2023-09-14 11:42:51 +0200

    Add constants for Carousel

---
+++ front/src/constants/index.js
@@ -0,0 +1,12 @@
+export const sectionTitleLogin = [
+    {
+        id: 1,
+        title: 'Scan It',
+        subTitle:'Professeur ou étudiant, utiliser votre téléphone comme feuille de présence !',
+    },
+    {
+        id: 2,
+        title: 'Check It',
+        subTitle:'Votre signature est confirmée quand vous scannez le QR code',
+    }
+]
