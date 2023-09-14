30c6d3a13fbb378a10363c52f249b192682d3235
anthony-mini <anthony-mini@outlook.fr>
2023-09-14 11:42:02 +0200

    Add Carousel components

--- front/src/components/index.js
+++ front/src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Header } from './Header';
 
 export { default as Login } from './Login';
+export { default as Carousel } from './Login/carousel';
 
 export { default as Qrcode } from './Qrcode';
